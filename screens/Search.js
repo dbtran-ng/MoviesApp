ed6c491c1c14b3a40c1be2ad80c5fbf98cd38e97
dbtran-ng <dinhbaotran.nguyen@sjsu.edu>
2022-02-21 00:20:24 -0800

    fix search function

--- screens/Search.js
+++ screens/Search.js
@@ -1,89 +1,96 @@
 import React, {useState} from 'react';
 import {
   SafeAreaView,
+  View,
   TextInput,
   StyleSheet,
-  View,
   TouchableOpacity,
   FlatList,
   Text,
 } from 'react-native';
-import Card from './../components/Card';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {searchMovieTv} from '../services/services';
+import Card from '../components/Card';
 import Error from '../components/Error';
+
 const Search = ({navigation}) => {
   const [text, onChangeText] = useState();
-  const [searchResult, setSearchResult] = useState();
-  const [error, setError] = useState();
+  const [searchResults, setSearchResults] = useState();
+  const [error, setError] = useState(false);
+
   const onSubmit = query => {
-    Promise.all([searchMovieTv(query, 'tv'), searchMovieTv(query, 'movie')])
+    Promise.all([searchMovieTv(query, 'movie'), searchMovieTv(query, 'tv')])
       .then(([movies, tv]) => {
         const data = [...movies, ...tv];
-        setSearchResult(data);
+        setSearchResults(data);
       })
       .catch(() => {
         setError(true);
       });
   };
+
   return (
     <React.Fragment>
-      <SafeAreaView style={{backgroundColor: 'transparent'}}>
-        <View>
-          <TextInput
-            style={styles.input}
-            placeholder="Search Movie or TV Show"
-            onChangeText={onChangeText}
-            value={text}
-          />
-        </View>
-        <View>
+      <SafeAreaView>
+        <View style={styles.container}>
+          <View style={styles.form}>
+            <TextInput
+              style={styles.input}
+              placeholder={'Search Movie or TV Show'}
+              onChangeText={onChangeText}
+              value={text}
+            />
+          </View>
           <TouchableOpacity
             onPress={() => {
               onSubmit(text);
             }}>
-            <Icon name={'search'} size={30} />
+            <Icon name={'search-outline'} size={30} />
           </TouchableOpacity>
         </View>
-        <View style={styles.searchItem}>
-          {searchResult && searchResult.length > 0 && (
+
+        <View style={styles.searchItems}>
+          {/* Searched items results */}
+          {searchResults && searchResults.length > 0 && (
             <FlatList
               numColumns={3}
-              data={searchResult}
-              renderItem={({item}) => {
-                <Card
-                  navigation={navigation}
-                  item={item}
-                  keyExtractor={item => item.id}
-                />;
-              }}
+              data={searchResults}
+              renderItem={({item}) => (
+                <Card navigation={navigation} item={item} />
+              )}
+              keyExtractor={item => item.id}
             />
           )}
 
-          {searchResult && searchResult.length === 0 && (
+          {/* When searched but no results */}
+          {searchResults && searchResults.length == 0 && (
             <View style={styles.noResults}>
-              <Text>No results found</Text>
+              <Text>No results matching your criteria.</Text>
+              <Text>Try different keywords.</Text>
             </View>
           )}
 
-          {!searchResult && (
+          {/* When nothing is searched */}
+          {!searchResults && (
             <View style={styles.empty}>
-              <Text>Type something...</Text>
+              <Text>Type something to start searching</Text>
             </View>
           )}
 
+          {/* Error */}
           {error && <Error />}
         </View>
       </SafeAreaView>
     </React.Fragment>
   );
 };
+
 const styles = StyleSheet.create({
   input: {
     borderRadius: 15,
     borderWidth: 0.5,
     height: 50,
-    padding: 10,
+    padding: 8,
   },
   container: {
     padding: 10,
@@ -96,11 +103,14 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     paddingRight: 8,
   },
-  searchItem: {
+
+  searchItems: {
     padding: 5,
   },
+
   noResults: {
     paddingTop: 20,
   },
 });
+
 export default Search;
